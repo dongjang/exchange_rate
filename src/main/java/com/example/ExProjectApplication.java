3af940a0210bafaebe5126c5e5f57ee94931c342
dongjang <66932888+dongjang@users.noreply.github.com>
2025-10-13 17:53:19 +0900

    시간 설정 수정

--- src/main/java/com/example/ExProjectApplication.java
+++ src/main/java/com/example/ExProjectApplication.java
@@ -1,10 +1,21 @@
 package com.example;
 
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExProjectApplication {
+    
+    @PostConstruct
+    public void init() {
+        // 애플리케이션 전역 시간대를 한국 시간으로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(ExProjectApplication.class, args);
     }
